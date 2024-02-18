8ca202de1e736d7698a8c72529cd6022e9c059d9
Vyacheslav <0907vk@gmail.com>
2024-02-18 14:31:42 +0200

    feat: hw-5

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const message = "Mango" + " is" + " happy";
+console.log(message);
+
+let result;
+
+result = 5 + "5";
+
+console.log(result);
+console.log(typeof result);
+
+result = 5 + "5" + 5;
+
+console.log(result);
+console.log(typeof result);
+
+result = 5 - "2";
+
+console.log(result);
+console.log(typeof result);
