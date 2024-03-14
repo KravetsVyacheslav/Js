6ae92d041fff847b11cb50fd7c590f8558734856
Vyacheslav <0907vk@gmail.com>
2024-03-14 18:38:37 +0200

    feat: hw-10 hw hw-11

---
+++ js/hw/hw-10.js
@@ -0,0 +1,52 @@
+const openModalButton = document.querySelector('[data-action="open-modal"]');
+const closeModalButton = document.querySelector('[data-action="close-modal"]');
+const modalBackdrop = document.querySelector('.backdrop');
+
+openModalButton.addEventListener('click', () => {
+/*   modalBackdrop.classList.add('show-modal'); */
+document.body.classList.add('show-modal')
+});
+
+closeModalButton.addEventListener('click', () => {
+/*   modalBackdrop.classList.remove('show-modal'); */
+document.body.classList.remove('show-modal')
+});
+
+modalBackdrop.addEventListener('click', (event) => {
+  if (event.target === modalBackdrop) {
+    modalBackdrop.classList.remove('show-modal');
+  }
+});
+
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
+
+nameInput.addEventListener('input', () => {
+  if (nameInput.value.trim() === '') {
+    nameOutput.textContent = 'незнайомець';
+  } else {
+    nameOutput.textContent = nameInput.value;
+  }
+});
+
+const validationInput = document.getElementById('validation-input');
+
+validationInput.addEventListener('input', () => {
+  const inputLength = validationInput.value.length;
+  const requiredLength = parseInt(validationInput.getAttribute('data-length'));
+
+  if (inputLength === requiredLength) {
+    validationInput.classList.remove('invalid');
+    validationInput.classList.add('valid');
+  } else {
+    validationInput.classList.remove('valid');
+    validationInput.classList.add('invalid');
+  }
+});
+
+const fontSizeControl = document.getElementById('font-size-control');
+const textSpan = document.getElementById('text');
+
+fontSizeControl.addEventListener('input', () => {
+  textSpan.style.fontSize = `${fontSizeControl.value}px`;
+});
---
+++ js/hw/hw-11.js
@@ -0,0 +1,56 @@
+function checkFields() {
+  let input1 = document.getElementById("input1").value;
+  let input2 = document.getElementById("input2").value;
+  if (input1 && input2) {
+    alert("Обидва поля заповнені");
+  } else {
+    alert("Поля не заповнені");
+  }
+}
+
+function checkSum() {
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
+  let sum = num1 + num2;
+  if (sum > 10) {
+    alert("Сума більша за 10");
+  } else {
+    alert("Сума менша 10");
+  }
+}
+
+function checkText() {
+  let text = document.getElementById("textInput").value;
+  if (text.includes("JavaScript")) {
+    alert("Текст містить слово JavaScript");
+  } else {
+    alert("Текст не містить слово JavaScript");
+  }
+}
+
+function checkNumber() {
+  let number = parseInt(document.getElementById("numberInput").value);
+  if (number > 10 && number < 20) {
+    alert("Число входить в діапазон від 10 до 20");
+  } else {
+    alert("Число не входить в діапазон від 10 до 20");
+  }
+}
+
+function validateForm() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  if (
+    name.length >= 3 &&
+    email.includes("@") &&
+    email.includes(".") &&
+    password.length >= 6
+  ) {
+    return true;
+  } else {
+    alert("Помилка: перевірте правильність заповнення форми!");
+    return false;
+  }
+}
